54a6b0c3e7edca37c686abd7a62eeaafc75abe07
Ángel M. Rayo <angelmrayoa@outlook.es>
2025-07-31 02:00:29 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-    # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
+    organization = "org-az9mr1io7my7fXiY"
     workspaces {
       name = "ace-iac-day-two"
     }
